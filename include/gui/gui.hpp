b6a0afd88dd384729073bdf2f4a0f375821ff1b1
Tristan Deschamps <tris790@gmail.com>
2021-04-26 15:27:40 -0400

    create direct2d renderer

---
+++ include/gui/gui.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <Windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <wchar.h>
+#include <math.h>
+
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+
+template<class Interface> inline void SafeRelease(
+    Interface **ppInterfaceToRelease)
+{
+    if (*ppInterfaceToRelease != NULL)
+    {
+        (*ppInterfaceToRelease)->Release();
+
+        (*ppInterfaceToRelease) = NULL;
+    }
+}
+
+class Gui {
+private:
+    HWND m_hwnd;
+    ID2D1Factory* m_pDirect2dFactory;
+    ID2D1HwndRenderTarget* m_pRenderTarget;
+    ID2D1SolidColorBrush* m_pLightSlateGrayBrush;
+    ID2D1SolidColorBrush* m_pCornflowerBlueBrush;
+
+    static LRESULT CALLBACK WndProc(
+        HWND hWnd,
+        UINT message,
+        WPARAM wParam,
+        LPARAM lParam);
+    HRESULT CreateDeviceIndependentResources();
+    HRESULT CreateDeviceResources();
+    void DiscardDeviceResources();
+    HRESULT OnRender();
+    void OnResize(UINT width, UINT height);
+
+public:
+    Gui():
+        m_hwnd(NULL),
+        m_pDirect2dFactory(NULL),
+        m_pRenderTarget(NULL),
+        m_pLightSlateGrayBrush(NULL),
+        m_pCornflowerBlueBrush(NULL)
+    {
+    };
+    ~Gui();
+    HRESULT initialize();
+    void runMessageLoop();
+};
--- src/app/tabbed.cpp
+++ src/app/tabbed.cpp
@@ -4,6 +4,7 @@
 #include <corrector/corrector.hpp>
 #include <automation/automation.hpp>
 #include <automation/ui_element_manager.hpp>
+#include <gui/gui.hpp>
 #include <string>
 
 #include <Windows.h>
@@ -12,76 +13,15 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    const wchar_t CLASS_NAME[] = L"Sample Window Class";
-
-    WNDCLASS wc = {};
-
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-
-    RegisterClass(&wc);
-
-    HWND hwnd = CreateWindowEx(
-        0,                           // Optional window styles.
-        CLASS_NAME,                  // Window class
-        L"Learn to Program Windows", // Window text
-        WS_OVERLAPPEDWINDOW,         // Window style
-        CW_USEDEFAULT,               // x
-        CW_USEDEFAULT,               // y
-        CW_USEDEFAULT,               // width
-        CW_USEDEFAULT,               // height
-        NULL,                        // Parent window
-        NULL,                        // Menu
-        hInstance,                   // Instance handle
-        NULL                         // Additional application data
-    );
-
-    if (hwnd == NULL)
-        return 0;
-
-    ShowWindow(hwnd, nCmdShow);
-    auto corrector = Corrector{};
-    auto text = L"My string";
-    auto output = corrector.autocomplete(text);
-    OutputDebugString(output.c_str());
-    auto uiElementManager = UIElementManager{};
-
-    auto test = [&](IUIAutomationElement *e) {
-        uiElementManager.setCurrentText(L"test");
-        OutputDebugString(L"Focus changed\n");
-    };
-
-    auto automator = Automation{test};
-
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0))
+    if (SUCCEEDED(CoInitialize(NULL)))
     {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        {
+            Gui app;
+            if (SUCCEEDED(app.initialize()))
+            {
+                app.runMessageLoop();
+            }
+        }
+        CoUninitialize();
     }
-
-    return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-        EndPaint(hwnd, &ps);
-    }
-        return 0;
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
